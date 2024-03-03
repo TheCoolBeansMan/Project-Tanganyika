aacc24ad218f13aff309d527cdb149232a9e5759
KitsuTurtlez <andresjbrito82499@gmail.com>
2024-03-03 15:00:11 -0500

    Implementing
    Implementing code

--- Assets/Scripts/CharacterInfo.cs
+++ Assets/Scripts/CharacterInfo.cs
@@ -67,4 +67,9 @@ public class CharacterInfo : MonoBehaviour
     {
 
     }
+
+    public void Deactivate()
+    {
+        this.enabled = false;
+    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,61 +12,135 @@ public class GameManager : MonoBehaviour
 
     public GameObject phaseButtonPlayer1;
     public GameObject phaseButtonPlayer2;
+    public GameObject player1TurnAnim;
+    public GameObject movePhaseAnim;
+    public GameObject navalPhaseAnim;
+    public GameObject artilleryPhaseAnim;
+    public GameObject player2TurnAnim;
 
     public List<GameObject> player1Units = new List<GameObject>();
     public List<GameObject> player2Units = new List<GameObject>();
+
+    private Animator player1PhaseAnimator;
+    private Animator player2PhaseAnimator;
+    private Animator movePhaseAnimator;
+    private Animator navalPhaseAnimator;
+    private Animator artilleryPhaseAnimator;
 
     private void Start()
     {
         currPhase = 1;
         currPlayer = 1;
         Debug.Log("Player: " + currPlayer.ToString() + ", Phase: " + currPhase.ToString());
+
+        // Get the Animator components
+        player1PhaseAnimator = player1TurnAnim.GetComponent<Animator>();
+        player2PhaseAnimator = player2TurnAnim.GetComponent<Animator>();
+        movePhaseAnimator = movePhaseAnim.GetComponent<Animator>();
+        navalPhaseAnimator = navalPhaseAnim.GetComponent<Animator>();
+        artilleryPhaseAnimator = artilleryPhaseAnim.GetComponent<Animator>();
+
+        player1TurnAnim.SetActive(false);
+        player2TurnAnim.SetActive(false);
+        movePhaseAnim.SetActive(false);
+        navalPhaseAnim.SetActive(false);
+        artilleryPhaseAnim.SetActive(false);
+
+        player1PhaseAnimator.enabled = false;
+        player2PhaseAnimator.enabled = false;
+        movePhaseAnimator.enabled = false;
+        navalPhaseAnimator.enabled = false;
+        artilleryPhaseAnimator.enabled = false;
+
+        //Instantiate(characterPrefab).GetComponent<CharacterInfo>();
+        //PositionCharacterOnTile(overlayTile);
+
+        foreach (GameObject unit in player1Units)
+        {
+            unit.gameObject.GetComponent<BoxCollider2D>().enabled = true;
+        }
     }
 
     private void FixedUpdate()
     {
         if (currPlayer % 2 != 0)
         {
-            phaseButtonPlayer1.gameObject.SetActive(true);
-            phaseButtonPlayer2.gameObject.SetActive(false);
-            if (currPhase == 1)
-            {
-                //Player 1 Transition Animation
-                //Move Phase Transition Animation
-
-            }
-            if (currPhase == 2)
-            {
-                //Naval Phase Transition Animation
-
-            }
-            if (currPhase == 3)
-            {
-                //Artillery Phase Transition Animation
-
-            }
+            PhaseAnim1();
         }
         if (currPlayer % 2 == 0)
         {
-            phaseButtonPlayer2.gameObject.SetActive(true);
-            phaseButtonPlayer1.gameObject.SetActive(false);
-            if (currPhase == 1)
-            {
-                //Player 2 Transition Animation
-                //Move Phase Transition Animation
-
-            }
-            if (currPhase == 2)
-            {
-                //Naval Phase Transition Animation
-
-            }
-            if (currPhase == 3)
-            {
-                //Artillery Phase Transition Animation
-
-            }
+            PhaseAnim2();
         }
+    }
+
+    public void PhaseAnim1()
+    {
+        phaseButtonPlayer1.gameObject.SetActive(true);
+        phaseButtonPlayer1.gameObject.GetComponent<Button>().interactable = true;
+        phaseButtonPlayer2.gameObject.SetActive(false);
+        phaseButtonPlayer2.gameObject.GetComponent<Button>().interactable = false;
+        if (currPhase == 1)
+        {
+            
+            // Player 1 Transition Animation
+            //player1TurnAnim.SetActive(true);
+            //player1PhaseAnimator.Play("BannerPanelOpacAnim");
+
+            // Move Phase Transition Animation
+            //movePhaseAnim.SetActive(true);
+            //movePhaseAnimator.Play("BannerPanelOpacAnim");
+        }
+        else if (currPhase == 2)
+        {
+            // Naval Phase Transition Animation
+            //player1TurnAnim.SetActive(false);
+            //movePhaseAnim.SetActive(false);
+            //navalPhaseAnim.SetActive(true);
+            //navalPhaseAnimator.Play("BannerPanelOpacAnim");
+        }
+        else if (currPhase == 3)
+        {
+            // Artillery Phase Transition Animation
+            //artilleryPhaseAnim.SetActive(true);
+            //artilleryPhaseAnimator.Play("BannerPanelOpacAnim");
+        }
+    }
+
+    public void PhaseAnim2()
+    {
+        phaseButtonPlayer2.gameObject.SetActive(true);
+        phaseButtonPlayer2.gameObject.GetComponent<Button>().interactable = true;
+        phaseButtonPlayer1.gameObject.SetActive(false);
+        phaseButtonPlayer1.gameObject.GetComponent<Button>().interactable = false;
+        if (currPhase == 1)
+        {
+            // Player 2 Transition Animation
+            //player2TurnAnim.SetActive(true);
+            //player1PhaseAnimator.Play("BannerPanelOpacAnim");
+
+            // Move Phase Transition Animation
+            //movePhaseAnim.SetActive(true);
+            //movePhaseAnimator.Play("BannerPanelOpacAnim");
+        }
+        else if (currPhase == 2)
+        {
+            // Naval Phase Transition Animation
+            //player2TurnAnim.SetActive(false);
+            //movePhaseAnim.SetActive(false);
+            //navalPhaseAnim.SetActive(true);
+            //navalPhaseAnimator.Play("BannerPanelOpacAnim");
+        }
+        else if (currPhase == 3)
+        {
+            // Artillery Phase Transition Animation
+            //artilleryPhaseAnim.SetActive(true);
+            //artilleryPhaseAnimator.Play("BannerPanelOpacAnim");
+        }
+    }
+
+    public void ClickTest()
+    {
+        Debug.Log("Click Confirmed");
     }
 
     public void EndPhase()
@@ -73,6 +150,11 @@ public class GameManager : MonoBehaviour
         {
             currPhase = 1;
             currPlayer++;
+
+            if (currPlayer > 2)
+            {
+                currPlayer = 1;
+            }
         }
         Debug.Log("Player: " + currPlayer.ToString() + ", Phase: " + currPhase.ToString());
     }
