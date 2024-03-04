856f9706c01e02e3d5ed73e2d73ec1f014cc4b39
KitsuTurtlez <andresjbrito82499@gmail.com>
2024-03-03 19:31:22 -0500

    CurrentChanges

--- Assets/Scripts/CharacterInfo.cs
+++ Assets/Scripts/CharacterInfo.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using UnityEngine.UI;
 
 public class CharacterInfo : MonoBehaviour
 {
     public OverlayTile activeTile;
+    public Tilemap battleMap;
+    public Tile currentTile;
     public GameObject actionMenu;
     private MouseController movementStatus;
     private RangeFinder attackRange;
     private List<OverlayTile> inRangeAttacks = new List<OverlayTile>();
+    private Vector3Int unitPosition;
+    private int xPos;
+    private int yPos;
+    private int zPos;
+
+    public GameObject raycastOrigin;
 
     public int unitMaxHP;
     public int unitCurrHP;
@@ -31,6 +40,16 @@ public class CharacterInfo : MonoBehaviour
         attackRange = new RangeFinder();
         movementStatus.GetComponent<MouseController>().character = this.GetComponent<CharacterInfo>();
         movementStatus.GetComponent<MouseController>().PositionCharacterOnTile(activeTile);
+    }
+
+    private void Update()
+    {
+        xPos = (int) transform.position.x;
+        yPos = (int) transform.position.y;
+        zPos = (int) transform.position.z;
+
+        unitPosition = new Vector3Int(xPos, yPos, zPos);
+        //currentTile = battleMap.GetTile(unitPosition);
     }
     private void OnMouseDown()
     {
@@ -66,6 +85,37 @@ public class CharacterInfo : MonoBehaviour
     public void UnitAttack()
     {
         
+        //// Check if the raycast origin is assigned
+        //if (raycastOrigin != null)
+        //{
+        //    float raycastLength = 10f;
+        //    Vector2 originPosition = raycastOrigin.transform.position;
+
+        //    // Raycast upwards from one tile above the raycast origin, detect objects with "GermUnit" tag
+        //    RaycastHit2D hitUp = Physics2D.Raycast(originPosition + Vector2.up, Vector2.up, raycastLength);
+        //    Debug.DrawRay(originPosition + Vector2.up, Vector2.up * raycastLength, Color.red, 1f); // Draw the ray for visualization
+
+        //    // Raycast downwards from one tile below the raycast origin, detect objects with "BritUnit" tag
+        //    RaycastHit2D hitDown = Physics2D.Raycast(originPosition - Vector2.up, Vector2.down, raycastLength);
+        //    Debug.DrawRay(originPosition - Vector2.up, Vector2.down * raycastLength, Color.blue, 1f); // Draw the ray for visualization
+
+        //    // Handle hit results if needed
+        //    if (hitUp.collider != null && hitUp.collider.tag == "GermUnit")
+        //    {
+        //        Debug.Log("Hit something above with GermUnit tag!");
+        //        // You can perform actions when hitting something above with GermUnit tag
+        //    }
+
+        //    if (hitDown.collider != null && hitDown.collider.tag == "BritUnit")
+        //    {
+        //        Debug.Log("Hit something below with BritUnit tag!");
+        //        // You can perform actions when hitting something below with BritUnit tag
+        //    }
+        //}
+        //else
+        //{
+        //    Debug.LogError("Raycast origin is not assigned!");
+        //}
     }
 
 
